e037ba782f7df6d96125b168ea39e324d2388ecb
DhaliwalSehaj <sehajpreet.dhaliwal@edu.sait.ca>
2023-10-11 22:18:06 -0600

    Week 4 commit to master

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Root from './root'
 import Week2_page from './week2/page'
 import Link from 'next/link'
 import ItemList from './week3/item-list'
+import NewItemSS from './week4/new-item'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Link href="/week2/" style={{ fontSize: 30,display:'flex',alignItems:'center',justifyContent:'center' }}>Week 2 Page</Link>
       
       <Link href="/week3/" style={{ fontSize: 30,display:'flex',alignItems:'center',justifyContent:'center' }}>Shopping List</Link>
+      <Link href="/week4/" style={{ fontSize: 30,display:'flex',alignItems:'center',justifyContent:'center' }}>Week 4</Link>
     </main>
   )
 }
